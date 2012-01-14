096de94c81eff09e5a86e25dad454a7f1b7b1509
Brian Tingle <brian.tingle.cdlib.org@gmail.com>
2012-01-14 15:43:20 -0800

    returns a basic dictionary of stats

--- ead_tag_use.py
+++ ead_tag_use.py
@@ -3,49 +3,74 @@
     Brian Tingle (c) 2012 UC Regents all rights reserved
     BSD license at bottom of file
 """
-# what order should the imports be in
-import sys
+					# standard modules
 import argparse
+from collections import defaultdict
 import fnmatch
-import os
 import glob
+import os
+import sys 				
+					# Pypi packages
+					# http://pypi.python.org/pypi/lxml
 from lxml import etree
 
-# load ead.dtd.xml into "dtd" global variable
-dtd = etree.parse('ead.dtd.xml');
+# load ead.dtd.xml into DTD constant
+DTD = etree.parse('ead.dtd.xml');
+# xml version of the DTD created with http://nwalsh.com/perl/dtdparse/
 
 def main(argv=None):
     # argument parser 
     parser = argparse.ArgumentParser(
         description='analyze EAD tag useage',
         epilog='recursively searches "*.xml" files in given dirs for EAD tags')
-    parser.add_argument('dir', nargs='+', 
-                     help='EAD XML corpus directory')
+    parser.add_argument('dir', nargs='+', help='EAD XML corpus directory')
     parser.add_argument('-outfile', nargs='?', type=argparse.FileType('w'),
-                     help='output tag useage report',
-                     default=sys.stdout)
-
+        help='output tag useage report', default=sys.stdout)
+    # parse the args from the command line if none were supplied from the REPL
     if argv is None:
         argv = parser.parse_args()
      
-    # set up dictionary to hold the stats
-    stats = {}
+    # set up dictionaries to hold the stats
+    # stats = {}
+    stats = defaultdict(int)
 
     # loop through all files
     # http://stackoverflow.com/questions/2212643/
-    # http://stackoverflow.com/questions/2186525
+    # http://stackoverflow.com/questions/2186525/
+    # multiple dir arguments can be supplied
     for dir in argv.dir:
+        # walk through all the files in the directory
         for root, subFolders, files in os.walk(dir):
+            # check the file has a .xml extension
             for filename in fnmatch.filter(files, '*.xml'):
                 filePath = os.path.join(root, filename)
-                stats = analyze_file(filePath, stats)
+                analyze_file(filePath, stats)
+    print stats
 
 def analyze_file(file, stats):
+    """analze the a file, update stats dictionary"""
+
+    # check that the file is not zero length
+    # http://stackoverflow.com/questions/2507808/
     if os.stat(file)[6]==0:
         return stats
-    print file, stats, dtd
     xml = etree.parse(file)
-    return stats
+    # recursivly decend the xml (modifies stats "in place")
+    count_elements(xml.getroot(),stats)
+
+def count_elements(node, stats):
+    """process the node, refering to the DTD, updating stats"""
+
+    # test that we are an element and not a comment nor processing instruction
+    if isinstance(node, (etree._Comment, etree._ProcessingInstruction)):
+        return
+
+    # count elements and attributes used based on DTD
+    stats[node.tag] += 1
+
+    for desc in list(node):
+        # recursive call
+        count_elements(desc, stats)
 
 # main() idiom for importing into read-eval-print loop for debugging 
 if __name__ == "__main__":
